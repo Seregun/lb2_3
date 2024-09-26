efadb82ae2b017d2c6ae020b22b2b711b20f6b96
Seregun <seregunmgp@gmail.com>
2024-09-26 10:52:46 +0300

    Update task3.py

--- lb2_3/task3.py
+++ lb2_3/task3.py
@@ -1,4 +1,4 @@
-import math;
-x = float(input("Введите объём шара: "));
-r = (3 * (x**(1/3)))/(4*math.pi);
-print("Радиус шара = ", r);
+import math
+x = float(input("Введите объём шара: "))
+r = (3 * (x**(1/3)))/(4*math.pi)
+print("Радиус шара = ", r)
